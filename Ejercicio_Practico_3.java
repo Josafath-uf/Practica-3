79bb5dc223dc82a00cc585b72681fbea64efbce1
Josafath-uf <minilu0789@gmail.com>
2025-03-19 19:36:17 -0600

    Add files via upload

---
+++ Ejercicio_Practico_3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_practico_3;
+
+/**
+ *
+ * @author minil
+ */
+public class Ejercicio_Practico_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Lunes;
+        String Martes;
+        String Miercoles;
+        String Jueves;
+        String Viernes;
+        String Sabado;
+        String Domingo;
+        
+        
+       String Productos[] = new String[3];
+       
+       
+    }
+
+    
+ 
+}
